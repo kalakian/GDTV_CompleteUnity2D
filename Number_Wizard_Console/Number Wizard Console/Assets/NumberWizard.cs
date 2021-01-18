af745ae32c92ccd14e1aeadc719251825eda9ad7
Derek Stobbs <dstobbs@googlemail.com>
2021-01-18 16:59:50 +0000

    Number Wizard Console: Refactor
    - Cleaned up the code a bit
    - Game restarts after number is guessed correctly

--- Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
     
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+
         Debug.Log("Welcome to number wizard, a simple guessing game");
         Debug.Log("Pick a number between " + min + " and " + max);
         Debug.Log("I'll try to guess it, so don't tell me what it is");
-        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        NextGuess();
         Debug.Log("Push Up if your number is higher, Push Down if it's lower, Push Enter if I got it correct");
-
-        max++;
     }
 
     // Update is called once per frame
@@ -25,19 +31,24 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("I guessed too low ... how about " + guess + "?");
+            min = guess + 1;
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("I guessed too high ... how about " + guess + "?");
+            max = guess - 1;
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("I guessed correctly! I knew I'd get there eventually :)");
-        }
+            StartGame();
+        }   
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your number higher or lower than " + guess + "?");
     }
 }
