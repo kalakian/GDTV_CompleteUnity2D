50e2ab4e00ae79ac0855f3a298d3e2edc06f9145
Derek Stobbs <dstobbs@googlemail.com>
2021-01-20 14:05:38 +0000

    Text101: Text update
    - Story text now updated via script (still placeholder though)

---
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "Hello World!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
