594c6d57e8315bf59cf16cb9b271f72bbd30e588
Derek Stobbs <dstobbs@googlemail.com>
2021-01-20 14:43:19 +0000

    Text101: Starting State Displayed
    - Story text now showing the text from the Starting State object

--- Text101/Text101/Assets/AdventureGame.cs
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -4,11 +4,15 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] private State startingState;
+
+    State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "Hello World!";
+        currentState = startingState;
+        textComponent.text = currentState.GetStateStory();
     }
 
     // Update is called once per frame
