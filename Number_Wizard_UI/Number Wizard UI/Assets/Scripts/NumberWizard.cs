95eb798075919fb2fe60b3c208e493524a00f5bb
Derek Stobbs <dstobbs@googlemail.com>
2021-01-22 15:58:06 +0000

    Number Wizard UI: Core Game Code
    - Imported and refactored the old Number Wizard Console code so it fits in with the new UI

---
+++ Number_Wizard_UI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
