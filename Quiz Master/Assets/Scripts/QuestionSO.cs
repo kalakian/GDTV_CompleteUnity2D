7b25b2b2777941113a3fcbfecd7dc1e92a8db31f
Derek Stobbs <dstobbs@googlemail.com>
2024-04-29 12:15:30 +0100

    Quiz Master - Questions
    * Added each of the questions in
    * First question is now displayed when the game starts

---
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        string answer = "";
+
+        if(index >= 0 && index < answers.Length)
+        {
+            answer = answers[index];
+        }
+
+        return answer;
+    }
+}
