80f91df020e980e93704edc1a07cf038df8883ed
Derek Stobbs <dstobbs@googlemail.com>
2024-05-03 15:01:26 +0100

    TileVania - Running & Jumping
    * Player now faces the direction they are running
    * Running animation now triggered
    * Player can jump

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,56 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5;
+    [SerializeField] float jumpSpeed = 5;
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider2D;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        int groundLayerIndex = LayerMask.GetMask("Ground");
+
+        if(value.isPressed && myCapsuleCollider2D.IsTouchingLayers(groundLayerIndex))
+        {
+            myRigidbody.velocity += new Vector2(0, jumpSpeed);
+        }
     }
 
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1);
+        }
     }
 }
