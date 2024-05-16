52f268ba447143c6277256030bbc93c8cc14e0f8
Derek Stobbs <dstobbs@googlemail.com>
2024-05-16 13:03:42 +0100

    Pixel Shmup - Player movement
    * Player can now move left, right, up, and down
    * Boundaries have been added to keep the player on the screen

---
+++ Pixel Shmup/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float paddingLeft = 0.5f;
+    [SerializeField] float paddingRight = 0.5f;
+    [SerializeField] float paddingTop = 0.5f;
+    [SerializeField] float paddingBottom = 0.5f;
+
+    Vector3 moveInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    void Move()
+    {
+        Vector2 newPos = transform.position + moveInput * moveSpeed * Time.deltaTime;
+
+        newPos.x = Mathf.Clamp(newPos.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(newPos.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+
+        transform.position = newPos;
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
